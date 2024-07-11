cf25ff02f4ed4658e7f10b8cc9a1ee9f3aa931bf
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-11 22:37:00 +0600

    compleate:home page done with out otp management system

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
    <div className="flex h-screen max-h-screen">
+    {/**TODO OTP Verification */}
     <section className="remove-scrollbar container my-auto">
     <div className="sub-container max-w-[496px]">
     <Image
@@ -15,8 +17,21 @@ export default function Home() {
     className="mb-12 h-10 w-fit"
     />
     <PatientForm/>
+    <div className="text-14-regular mt-20 flex justify-between">
+    <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Care Pluse</p>
+    <Link href='/?admin=true' className="text-green-500"> 
+    Admin
+    </Link>
+    </div>
     </div>
     </section>
+    <Image
+    src='/assets/images/onboarding-img.png'
+    width={1000}
+    height={1000}
+    alt=""
+    className="side-img max-w-[50%]"
+    />
    </div>
   );
 }
