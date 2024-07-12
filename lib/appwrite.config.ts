31172f195182dcbd5f8468eb12f5cd050613e01e
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-12 23:44:56 +0600

    adding

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,5 +1,8 @@
+import * as sdk from 'node-appwrite'
 const {PROJECT_ID,
     DATABASE,
     PATIENTS_COLLECTION_ID,
     DOCTORS_COLLECTION_ID,
     APPIONTMENT_COLLECTION_ID,NEXT_PUBLIC_BUCKET_ID:BUCKET_ID,Main} = process.env
+    
+const client = new sdk.Client();
